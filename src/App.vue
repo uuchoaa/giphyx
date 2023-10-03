637267c297537a31ef454fcab3b0b1dc1485af27
Rafael Uchôa <uchoa@gmail.com>
2023-10-03 20:52:54 -0300

    feat: add some layout with tailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-    <div>
-      <div 
-        v-for="gif in gifs" 
-        :key="gif.id" 
-      >
-        <img :src="gif.images.fixed_height.url" :alt="gif.title" />
+    
+    <!-- Header -->
+    <div class="bg-blue-600 text-white text-center py-4">
+        <h1 class="text-2xl font-bold">Giphy Search App</h1>
+    </div>
+
+    <!-- GIF Grid -->
+    <div class="px-4 mt-6">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        
+        <div
+          v-for="gif in gifs" 
+          :key="gif.id" 
+          class="group aspect-h-7 h-48 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
+        >
+          <img :src="gif.images.fixed_height.url" :alt="gif.title" />
+        </div>
+
       </div>
     </div>
+
   </div>
 </template>
 
@@ -23,7 +36,7 @@ export default {
   methods: {
     async searchGifs() {
       const api_key = process.env.VUE_APP_GIPHY_API
-      const response = await Axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`);
+      const response = await Axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=5`);
       this.gifs = response.data.data;
 
     },
