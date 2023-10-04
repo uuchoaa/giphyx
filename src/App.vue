f12ce4261fe26bfd48a0093a336780de6c29a2fe
Rafael Uchôa <uchoa@gmail.com>
2023-10-03 21:31:25 -0300

    refactor: add grid component

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,9 @@
       <SearchComponent @search="searchGifs" />
     </div>
 
-
     <!-- GIF Grid -->
     <div class="px-4 mt-6">
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        
-        <div
-          v-for="gif in gifs" 
-          :key="gif.id" 
-          class="group aspect-h-7 h-48 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
-        >
-          <img :src="gif.images.fixed_height.url" :alt="gif.title" />
-        </div>
-
-      </div>
+      <GridComponent :gifs="gifs" />
     </div>
 
   </div>
@@ -33,11 +22,13 @@
 <script>
 import Axios from 'axios';
 import SearchComponent from './components/SearchComponent';
+import GridComponent from './components/GridComponent';
 
 
 export default {
   components: {
     SearchComponent,
+    GridComponent,
   },
   data() {
     return {
@@ -46,7 +37,6 @@ export default {
   },
   methods: {
     async searchGifs(query) {
-      console.log(query)
       const api_key = process.env.VUE_APP_GIPHY_API
 
       const default_params = {
---
+++ src/components/GridComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    
+    <div
+      v-for="gif in gifs" 
+      :key="gif.id" 
+      class="group aspect-h-7 h-48 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
+    >
+      <img :src="gif.images.fixed_height.url" :alt="gif.title" />
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['gifs'],
+};
+</script>
